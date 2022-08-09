3682adc4904b9d6b8694b1d03380ea7def6bdb1b
necelentano <necelentano@gmail.com>
2022-08-10 00:23:21 +0300

    Added Post Section & some fixes in sanity.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
 
-import { sanityClient } from '../sanity'
+import { sanityClient, urlFor } from '../sanity'
 
 import {Post} from '../types'
 
@@ -25,6 +26,26 @@ const Home = ({posts}: Props) => {
           <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
         </div>
         <img className="hidden md:inline-flex h-32 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="Medium logo" />
+      </div>
+
+      {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+      {posts?.map(post => (
+        <Link key={post._id} href={`/post/${
+          post.slug.current
+        }`}>
+          <div className="group cursor-pointer border rounded-lg overflow-hidden">
+            <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt="" />
+            <div className="flex justify-between bg-white p-5">
+              <div>
+                <p className="text-lg font-bold">{post.title}</p>
+                <p className="text-xs">{post.description} by {post.author.name}</p>
+              </div>
+              <img src={urlFor(post.author.image).url()!} alt="" className="h-12 w-12 rounded-full"/>
+            </div>
+          </div>
+        </Link>
+      ))}
       </div>
     </div>
   )
--- sanity.js
+++ sanity.js
@@ -1,4 +1,5 @@
-import {createImageBuilder, createCurrentUserHook, createClient} from 'next-sanity'
+import {createCurrentUserHook, createClient} from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
 
@@ -22,7 +23,7 @@ export const sanityClient = createClient(config)
 // Set up a helper function for generating Image URLs with only the asset
 // reference data in your documents
 // Read more: https://www.sanity.io/docs/image-url
-export const urlFor = (source) => createImageBuilder(config).image(source)
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
 
 // Helper function for using the current logged in user account
 export const useCurrentUser = createCurrentUserHook(config)
